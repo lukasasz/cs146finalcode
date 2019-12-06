57b45b1316cd30629cdf949d07344893982ef3ae
lukasasz <55508557+lukasasz@users.noreply.github.com>
2019-12-06 15:55:23 -0500

    Add files via upload

---
+++ javaproject.js
@@ -0,0 +1,35 @@
+function F11() {
+    alert("364 Calories")
+  }
+
+  function F12() {
+    alert("564 Calories")
+  }
+
+  function F13() {
+    alert("400 Calories")
+  }
+
+  function F21() {
+    alert("239 Calories")
+  }
+
+  function F22() {
+    alert("439 Calories")
+  }
+
+  function F23() {
+    alert("275 Calories")
+  }
+
+  function F31() {
+    alert("555 Calories")
+  }
+
+  function F32() {
+    alert("755 Calories")
+  }
+
+  function F33() {
+    alert("591 Calories")
+  }
