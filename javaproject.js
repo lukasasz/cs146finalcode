de47c6c6f1e454bcddc95e9f9416c7d07b25e3a7
D'Antoine Newsome <dlnews99@yahoo.com>
2019-12-07 00:24:14 -0500

    DuckDiet Final
    FILES TO USE: index.html // choosemeal.html // contact.html // exercise1.html // javaproject.js // ourcss_update.css // EASHall.jpg // duckdietlogo.png

--- javaproject.js
+++ javaproject.js
@@ -33,3 +33,74 @@ function F11() {
   function F33() {
     alert("591 Calories")
   }
+
+  /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
+
+var BFChoice = ""
+var LHChoice = ""
+var DNChoice = ""
+
+  var dict = {
+    "Eggs" : 100,
+    "Hash Browns" : 470,
+    "Cereal" : 300,
+    "Sandwich" : 340,
+    "Pasta" : 80,
+    "Fruit" : 100,
+    "Steak" : 680,
+    "Burrito" : 450,
+    "Pizza" : 300,
+  }
+
+var calories = 0
+
+  function addCalories() {
+    if(document.getElementById("choices").value != null){
+    BFChoice = document.getElementById("choices").value
+    }
+
+    if(document.getElementById("lunchchoices").value != null){
+    LHChoice = document.getElementById("lunchchoices").value
+    }
+
+    if(document.getElementById("lunchchoices").value != null){
+    DNChoice = document.getElementById("dinnerchoices").value
+    }
+
+    if (BFChoice in dict) {
+      calories += dict[BFChoice]
+    }
+    if (LHChoice in dict) {
+      calories += dict[LHChoice]
+    }
+    if (DNChoice in dict) {
+      calories += dict[DNChoice]
+    }
+    localStorage.setItem("calories", calories)
+    return calories
+  }
+
+  function subCaloriesBall() {
+    var t = localStorage.getItem("calories") - 300
+    alert(t)
+  }
+
+  function subCaloriesRun() {
+    var r = localStorage.getItem("calories") - 100
+    alert(r)
+  }
+
+  function subCaloriesCycle() {
+    var v = localStorage.getItem("calories") - 264
+    alert(v)
+  }
